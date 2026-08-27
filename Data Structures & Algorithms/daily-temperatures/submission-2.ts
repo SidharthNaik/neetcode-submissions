aae7e6f748ea0624a19cfcceecb1bba732ec948e
Sidharth Suresh Naik <sn.sidharth@gmail.com>
2026-08-27 08:16:09 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.ts
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        // MaxStack Approach
+        // Keep a MaxStack from the right end
+        // number and pos
+        // if higher than current sub curr pos - that pos and then add to result
+        const tempsLen = temperatures.length;
+        if(tempsLen === 1) return [0];
+        const result = [0]
+        const tempStack = [temperatures[tempsLen-1]]
+        const posStack = [tempsLen-1]
+        // console.log(temperatures[temperatures.length-2])
+        // console.log(posStack)
+        let size = 0
+        for(let i = tempsLen-2; i>=0; i--){
+            const temp = temperatures[i]
+            while(size >=0 && tempStack[size] <= temp){
+                tempStack.pop()
+                posStack.pop()
+                size--
+            }
+            // console.log(tempStack)
+            // console.log(posStack)
+            if(size<0){
+                result.push(0)
+            } else {
+                result.push(posStack[size] - i)
+            }
+            size++
+            tempStack.push(temp)
+            posStack.push(i)
+        }
+        return result.reverse()
+    }
+}
